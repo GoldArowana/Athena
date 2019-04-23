30561c6a6cae71f464d1b6eae3d4ec0a52a9b0f8
arowana <arowana.java@qq.com>
2019-04-23 16:39:29 +0800

    :art: 调整代码格式

---
+++ athena-sal/src/main/java/com/aries/department/athena/sal/App.java
@@ -0,0 +1,10 @@
+package com.aries.department.athena.sal;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
