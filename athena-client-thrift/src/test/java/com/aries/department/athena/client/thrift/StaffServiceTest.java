cb9be4cbbd8d579e27c7792d6573502b601c3f01
arowana <arowana.java@qq.com>
2019-05-05 11:02:42 +0800

    :bug: server端调不通，因为打包没打数据库配置，导致无法创建hikari连接池

--- athena-client-thrift/src/test/java/com/aries/department/athena/client/thrift/StaffServiceTest.java
+++ athena-client-thrift/src/test/java/com/aries/department/athena/client/thrift/StaffServiceTest.java
@@ -1,33 +1,30 @@
 package com.aries.department.athena.client.thrift;
 
-import com.aries.department.athena.contract.thrift.service.DepartmentService;
+import com.aries.department.athena.contract.thrift.dto.CompanyInfo;
+import com.aries.department.athena.contract.thrift.service.StaffService;
 import com.aries.hera.client.thrift.ThriftHelper;
+import org.apache.thrift.TException;
 import org.junit.Test;
 
 public class StaffServiceTest {
     @Test
     public void pingTest() throws Exception {
-//        TTransport transport = new TSocket(athena.getHost(), athena.getPort());
-//        TProtocol protocol = new TBinaryProtocol(transport);
-//        TMultiplexedProtocol multiplexedProtocol = new TMultiplexedProtocol(protocol, "StaffService");
-//        StaffService.Client client = new StaffService.Client(multiplexedProtocol);
-//
-//        transport.open();
-//        String ping = client.ping();
-//        System.out.println(ping);
-//        transport.close();
+//        List<StaffInfo> staffInfoList = StaffFacade.batchQeury(0, 1, 20);
+//        System.out.println(staffInfoList);
 
-//        ServiceInfo athena = DiscoverClient.getFirstService("athena");
-//        call((client) -> {
-//            try {
-//                client.ping();
-//            } catch (TException e) {
-//                e.printStackTrace();
-//            }
-//            return "";
-//        });
-//        System.out.println(athena);
-        ThriftHelper.call("athena", DepartmentService.Client.class, client -> client.ping());
+        CompanyInfo companyInfo = new CompanyInfo();
+        companyInfo.setName("aries");
+        companyInfo.setPassword("123456");
+        Long count = ThriftHelper.call(StaffService.Client.class, client -> {
+            try {
+                return client.getStaffCountByDepartmentId(companyInfo, 0L);
+            } catch (TException e) {
+                e.printStackTrace();
+                return 100L;
+            }
+        }, "localhost", 6001);
+
+        System.out.println(count);
     }
 
 }
